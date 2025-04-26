983f1141911708ba048e589068ffb16a997bc066
Alex Kates <alex.kates@croissant.com>
2025-04-25 20:34:42 -0400

    refactor: update QueryList component to use Card and CardHeader for better UI structure

--- src/components/query-list.tsx
+++ src/components/query-list.tsx
@@ -1,28 +1,23 @@
 import type { Query } from "src/types/query";
+import { Card, CardHeader } from "./ui/card";
 
+// TODO: I don't know why, but the type of queries is not right.
 export default function QueryList({ queries }: { queries?: Query[] | undefined }) {
   if (!queries || queries.length === 0) {
-    return (
-      <div className="flex justify-center items-center p-4 text-gray-500">
-        No queries available
-      </div>
-    );
+    return <div className="flex justify-center items-center p-4 text-gray-500">No queries available</div>;
   }
 
   return (
     <div className="flex flex-col gap-2">
       {queries.map((query) => (
-        <div
-          key={query.url}
-          className="flex items-center justify-between p-2 border-b border-gray-200"
-        >
-          <div className="flex flex-col">
-            <span className="text-sm font-semibold">{query.url}</span>
-            <span className="text-xs text-gray-500">
-              {new Date(query.createdAt).toLocaleString()}
-            </span>
-          </div>
-        </div>
+        <Card key={query.url}>
+          <CardHeader>
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold truncate max-w-sm">{query.name ?? "Query from"}</span>
+              <span className="text-xs text-gray-500">{new Date(query.createdAt).toLocaleString()}</span>
+            </div>
+          </CardHeader>
+        </Card>
       ))}
     </div>
   );

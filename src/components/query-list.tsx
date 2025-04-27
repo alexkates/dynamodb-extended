7c57ee137dff458f9b23a615178cae1d6978cd7e
Alex Kates <alex.kates@croissant.com>
2025-04-27 15:07:24 -0400

    feat: enhance QueryList to display a message and button when no queries are available

--- src/components/query-list.tsx
+++ src/components/query-list.tsx
@@ -1,14 +1,37 @@
 import type { Query } from "src/types/query";
 import QueryListItem from "./query-list-item";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import { Button } from "./ui/button";
+import { updateCurrentTabUrlAndForceReload } from "src/utils/tabs";
 
 interface Props {
   queries: Query[] | undefined;
 }
 
-// TODO: I don't know why, but the type of queries is not right.
 export default function QueryList({ queries }: Props) {
   if (!queries || queries.length === 0) {
-    return <div className="flex justify-center items-center p-4">No queries available</div>;
+    function onGetStartedClicked() {
+      updateCurrentTabUrlAndForceReload("https://console.aws.amazon.com/dynamodbv2/home");
+    }
+
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Card>
+          <CardHeader>
+            <CardTitle>DynamoDB Extended</CardTitle>
+            <CardDescription>A Chrome extension to enhance your DynamoDB Console experience.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p>To get started, simply open your DynamoDB Console and run queries as you normally would. Your query history will appear here automatically.</p>
+          </CardContent>
+          <CardFooter>
+            <Button variant="default" size="lg" onClick={() => onGetStartedClicked()}>
+              Start Querying
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    );
   }
 
   return (
